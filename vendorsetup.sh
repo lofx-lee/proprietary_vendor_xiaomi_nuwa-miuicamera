b3cf631a19e4a77d1a41e9395a56e2fa2b7d4943
lofx-lee <mutongbaobao@gmail.com>
2026-07-29 15:49:07 +0800

    nuwa-miuicamera: Initial MiuiCamera from NUWA-OS3.0.305.0.WMBCNXM

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to dynamically reassemble .part* files
+
+for file in $(find "vendor/xiaomi/nuwa-miuicamera" -type f -name "*.part00"); do
+    base_name=${file%.part00}
+    cat ${base_name}.part* > $base_name
+    echo "Reassembly of $base_name complete!"
+done
